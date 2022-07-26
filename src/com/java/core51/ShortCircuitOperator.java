b124b1530241bc028ee7d0f14b213e259970b644
Maruf <maruf.tcld@gmail.com>
2022-07-26 12:25:13 +0600

    commit test

---
+++ src/com/java/core51/ShortCircuitOperator.java
@@ -0,0 +1,11 @@
+package com.java.core51;
+
+public class ShortCircuitOperator {
+    public static void main(String[] args) {
+        if(true){
+        
+        }else{
+        
+        }
+    }
+}
